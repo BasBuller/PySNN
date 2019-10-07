9652976a7264edd78ea2cd36dcab1444fa302695
Bas Buller <bas.buller@gmail.com>
2019-10-07 10:50:45 +0200

    Simplifying learning rule constructor

--- pysnn/learning.py
+++ pysnn/learning.py
@@ -1,6 +1,5 @@
 import torch
 import torch.nn as nn
-from torch.nn.parameter import Parameter
 
 from pysnn.connection import Connection
 from pysnn.utils import _set_no_grad, tensor_clamp
@@ -10,17 +9,16 @@ from pysnn.utils import _set_no_grad, tensor_clamp
 # Learning rule base class
 #########################################################
 class LearningRule(nn.Module):
-    r"""Base class for correlation based learning rules in spiking neural networks."""
+    r"""Base class for correlation based learning rules in spiking neural networks.
+    
+    Each 'layer' (combination of a Connection and a Neuron object) needs a separate LearningRule object.
+    """
     # TODO: use Black (install it) and see whether this changes back to correct
     def __init__(self,
                  connection,
-                 pre_neuron,
-                 post_neuron,
                  lr):
         super(LearningRule, self).__init__()
         self.connection = connection
-        self.pre_neuron = pre_neuron
-        self.post_neuron = post_neuron
         self.lr = lr
 
     def no_grad(self):
@@ -37,27 +35,11 @@ class LearningRule(nn.Module):
 #########################################################
 # Fede STDP
 #########################################################
-def _fede_ltp_ltd(w, w_init, trace, a):
-    dw = w - w_init
-
-    # LTP computation
-    ltp_w = torch.exp(-dw)
-    ltp_t = torch.exp(trace) - a
-    ltp = ltp_w * ltp_t
-
-    # LTD computation
-    ltd_w = -torch.exp(dw)
-    ltd_t = torch.exp(1 - trace) - a
-    ltd = ltd_w * ltd_t
-
-    return ltp, ltd
-
-
 class FedeSTDP(LearningRule):
     r"""STDP version for Paredes Valles, performs mean operation over the batch 
     dimension before weight update."""
-    def __init__(self, connection, pre_neuron, post_neuron, lr, w_init, a):
-        super(FedeSTDP, self).__init__(connection, pre_neuron, post_neuron, lr)
+    def __init__(self, connection, lr, w_init, a):
+        super(FedeSTDP, self).__init__(connection, lr)
         self.w_init = torch.tensor(w_init, dtype=torch.float)
         self.a = torch.tensor(a, dtype=torch.float)
 
@@ -68,28 +50,32 @@ class FedeSTDP(LearningRule):
         trace = self.connection.trace.data.view(-1, *w.shape)
 
         # LTP and LTD
-        ltp, ltd = _fede_ltp_ltd(w, self.w_init, trace, self.a)
+        dw = w - self.w_init
+
+        # LTP computation
+        ltp_w = torch.exp(-dw)
+        ltp_t = torch.exp(trace) - self.a
+        ltp = ltp_w * ltp_t
+
+        # LTD computation
+        ltd_w = -torch.exp(dw)
+        ltd_t = torch.exp(1 - trace) - self.a
+        ltd = ltd_w * ltd_t
 
         # Perform weight update
-        self.connection.weight.data += self.lr * (ltp + ltd).mean(0)
+        self.connection.weight += self.lr * (ltp + ltd).mean(0)
 
 
 #########################################################
 # MSTDPET
 #########################################################
 class MSTDPET(LearningRule):
-    r"""Apply MSTDPET from (Florian 2007) to the provided connections.
-    
-    Uses just a single, scalar reward value.
-    Update rule can be applied at any desired time step.
-
-    The connection parameter is a list of dictionaries, each dict containing the following keys:
-        - weights: weight Parameter for connection.
-        - pre_syn_trace: pre synaptic traces.
-        - post_syn_trace: post synaptic traces.
-    """
+    r"""Apply MSTDPET from (Florian 2007) to the provided connections."""
     def __init__(self, connection, pre_neuron, post_neuron, lr, dt, e_trace_decay):
-        super(MSTDPET, self).__init__(connection, pre_neuron, post_neuron, lr)
+        super(MSTDPET, self).__init__(connection, post_neuron, lr)
+        self.pre_neuron = pre_neuron
+        self.post_neuron = post_neuron
+
         self.dt = dt
         self.e_trace_decay = e_trace_decay
 
@@ -107,31 +93,5 @@ class MSTDPET(LearningRule):
         self.e_trace += self.pre_neuron.trace * self.post_neuron.spiking() - self.post_neuron.trace * self.pre_neuron.spiking()
 
     def forward(self, reward):
-        # TODO: why would one use .data? Also done in FedeSTDP
         # TODO: add weight clamping?
-        # TODO: not sure whether dt belongs here
-        self.connection.weight.data += self.lr * self.dt * reward * self.e_trace
-            
-
-#########################################################
-# Additive RSTDP
-#########################################################
-class AdditiveRSTDPLinear(LearningRule):
-    r"""Basic, additive RSTDP formulation for linear layers.
-    
-    Can be used for many published learning rules, the difference lies in the trace formulations.
-    Use a neuron with the desired trace formulation in conjunction with this learning rule.
-    """
-    def __init__(self, connection, pre_neuron, post_neuron, lr, dt):
-        super(AdditiveRSTDPLinear, self).__init__(connection, pre_neuron, post_neuron, lr)
-        self.dt = dt
-
-        self.init_rule()
-
-    def forward(self, reward):
-        # TODO: needs transpose of second
-        # TODO: pre - post or other way around?
-        delta_trace = self.pre_neuron.trace - self.post_neuron.trace
-        # TODO: not sure whether dt belongs here
-        # TODO: needs weight clamp
-        self.connection.weight += self.lr * self.dt * reward * delta_trace
+        self.connection.weight += self.lr * reward * self.e_trace
