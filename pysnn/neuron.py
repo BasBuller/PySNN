5a39ea91859bc36b6e1780462cbddbe9044f522a
Bas Buller <bas.buller@gmail.com>
2020-02-26 09:25:09 +0100

    small change to neuron

--- pysnn/neuron.py
+++ pysnn/neuron.py
@@ -177,13 +177,15 @@ class BaseNeuron(nn.Module):
 
         # Store shape for easy use
         self.cells_shape = torch.tensor(cells_shape)
+        param_shape = cells_shape[1:]
+        self.param_shape = torch.tensor(param_shape)
 
         # Fixed parameters
         self.register_buffer("v_rest", torch.tensor(v_rest, dtype=torch.float))
         self.register_buffer("dt", torch.tensor(dt, dtype=torch.float))
         self.register_buffer("duration_refrac", duration_refrac)
-        self.register_buffer(
-            "thresh_center", thresh * torch.ones(cells_shape, dtype=torch.float)
+        self.thresh_center = Parameter(
+            thresh * torch.ones(param_shape, dtype=torch.float), requires_grad=False
         )
 
         # Define dynamic parameters
@@ -262,6 +264,7 @@ class BaseNeuron(nn.Module):
             self.complete_trace = torch.zeros(
                 *self.v_cell.shape, 1, device=self.v_cell.device
             ).bool()
+        self.reset_thresh()
 
     def reset_thresh(self):
         r"""Reset threshold to initialization values, allows for different standard thresholds per neuron."""
